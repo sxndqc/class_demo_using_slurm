33ff5bd0d43bd6e293a930ffd1dc1ce1be101912
SUN Haibo <sxndqc@users.noreply.github.com>
2025-11-14 01:53:25 -0500

    Revise SLURM job script comments for clarity
    Updated job configuration comments in run.sh for clarity.

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=demo               # name of your job
-#SBATCH --output=logs/%j_finetune.log # std and log
-#SBATCH --error=logs/%j_finetune.log  # error log
-#SBATCH --nodelist=student-gpu-001    # which node
-#SBATCH --gres=gpu:1                  # num of gpus
-#SBATCH --cpus-per-task=10            # num of cpus
-#SBATCH --mem=96G                     # memory to use
-#SBATCH --time=24:00:00               # running time (HH:MM:SS)
+#SBATCH --output=logs/%j_demo.log     # std and log, suggested to be put in a special "log/" folder to make it clean.
+#SBATCH --error=logs/%j_demo.log      # error log, suggested to come along with standard output and warning logs.
+#SBATCH --nodelist=student-gpu-001    # which node, use 1 and 2 dedicated for the class.
+#SBATCH --gres=gpu:1                  # num of gpus, don't apply for more than 1 because (1) 1 GPU is enough for your assignment (2) other students need to use them (3) Distributed training means a totally different script writing.
+#SBATCH --cpus-per-task=10            # num of cpus, don't apply for too many because they are NOT shared.
+#SBATCH --mem=96G                     # memory to use, it's fine to ask for the maximum because it is shared.
+#SBATCH --time=24:00:00               # running time (HH:MM:SS), you can ask for the maximum time but normally your script should finish in 10 hours.
 
 # Step 1: login to the cluster: ask for permission to add your name to user group (SHA256 key)
 # Step 2: sync your files to the cluster
