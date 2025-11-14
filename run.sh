61142778ca75a4d255c709609af4c251b0149bfc
SUN Haibo <sxndqc@users.noreply.github.com>
2025-11-14 01:34:00 -0500

    Update SLURM job configuration in run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+# Step 1: login to the cluster: ask for permission to add your name to user group (SHA256 key)
+# Step 2: sync your files to the cluster
+# Step 3: try to run your job with interactive pty for debugging
+# Step 4: submit your job to handover
+# Step 5: Use /data/$USER_NAME to store your model (which is fine for this class)
+# Step 6: Etiquette for using shared cluster resources:
+    # Be mindful of resource usage and avoid monopolizing resources.
+    # Regularly monitor your jobs and terminate any that are no longer needed.
+    # Clean up temporary files and data after job completion to free up space.
+
+
+
+#!/bin/bash
+#SBATCH --job-name=demo               # name of your job
+#SBATCH --output=logs/%j_finetune.log # std and log
+#SBATCH --error=logs/%j_finetune.log  # error log
+#SBATCH --nodelist=student-gpu-001    # which node
+#SBATCH --gres=gpu:1                  # num of gpus
+#SBATCH --cpus-per-task=10            # num of cpus
+#SBATCH --mem=96G                     # memory to use
+#SBATCH --time=24:00:00               # running time (HH:MM:SS)
+
+python class_demo.py
